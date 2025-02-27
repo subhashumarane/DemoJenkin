dcbd8f1d481f98d01412e9ff70f410ec1b818db0
Subhash Umarane <Subhash Umarane@Subhash>
2025-02-27 18:59:24 +0530

    Second commit

---
+++ DemoJenkins/src/test/java/healthCheckUp/gitCommitPractice.java
@@ -0,0 +1,12 @@
+package healthCheckUp;
+
+public class gitCommitPractice {
+	public static void main(String[] args) {
+		//later implementation;
+	}
+	public void gitPush()
+	{
+		//later implementation;
+	}
+
+}
